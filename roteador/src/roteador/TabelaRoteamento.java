cf6064e70dd3098bc780e59c0fcb5628e7ca021d
Cassiano <cassiano.flores2424@gmail.com>
2023-05-24 19:04:03 -0300

    tabela

--- roteador/src/roteador/TabelaRoteamento.java
+++ roteador/src/roteador/TabelaRoteamento.java
@@ -1,32 +1,67 @@
 package roteador;
 
 import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TabelaRoteamento {
-    /*Implemente uma estrutura de dados para manter a tabela de roteamento. 
-     * A tabela deve possuir: IP Destino, Métrica e IP de Saída.
-    */
-    
-    public TabelaRoteamento(){
-    
-    }
-    
-    
-    public void update_tabela(String tabela_s,  InetAddress IPAddress){
-        /* Atualize a tabela de rotamento a partir da string recebida. */
-        
-        System.out.println( IPAddress.getHostAddress() + ": " + tabela_s);
-    
-    }
-    
-    public String get_tabela_string(){
-        String tabela_string = "!"; /* Tabela de roteamento vazia conforme especificado no protocolo */
-        
-        /* Converta a tabela de rotamento para string, conforme formato definido no protocolo . */
-        
-        return tabela_string;
-    }
-    
 
-    
+    private Map<String, Rota> tabela;
+
+    public TabelaRoteamento() {
+        tabela = new HashMap<>();
+    }
+
+    public synchronized void updateTabela(String tabelaS, InetAddress ipAddress) {
+        System.out.println(ipAddress.getHostAddress() + ": " + tabelaS);
+        String[] rotas = tabelaS.split("\\*");
+
+        for (String rota : rotas) {
+            String[] campos = rota.split(";");
+            if (campos.length == 2) {
+                String ipDestino = campos[0];
+                int metrica = Integer.parseInt(campos[1]);
+                String ipSaida = ipAddress.getHostAddress();
+
+                if (tabela.containsKey(ipDestino)) {
+                    Rota rotaExistente = tabela.get(ipDestino);
+                    if (metrica < rotaExistente.metrica) {
+                        rotaExistente.metrica = metrica;
+                        rotaExistente.ipSaida = ipSaida;
+                        System.out.println("Rota atualizada: " + ipDestino + " Métrica: " + metrica + " IP de Saída: " + ipSaida);
+                    }
+                } else {
+                    tabela.put(ipDestino, new Rota(metrica, ipSaida));
+                    System.out.println("Nova rota adicionada: " + ipDestino + " Métrica: " + metrica + " IP de Saída: " + ipSaida);
+                }
+            }
+        }
+    }
+
+    public synchronized void removerRota(String ipDestino) {
+        if (tabela.containsKey(ipDestino)) {
+            tabela.remove(ipDestino);
+            System.out.println("Rota removida: " + ipDestino);
+        }
+    }
+
+    public synchronized String getTabelaString() {
+        StringBuilder tabelaString = new StringBuilder();
+        for (Map.Entry<String, Rota> entry : tabela.entrySet()) {
+            String ipDestino = entry.getKey();
+            Rota rota = entry.getValue();
+            tabelaString.append("*").append(ipDestino).append(";").append(rota.metrica);
+        }
+        return tabelaString.toString();
+    }
+
+    private class Rota {
+        private int metrica;
+        private String ipSaida;
+
+        public Rota(int metrica, String ipSaida) {
+            this.metrica = metrica;
+            this.ipSaida = ipSaida;
+        }
+    }
 }
