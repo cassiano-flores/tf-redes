5aa5600895276b8fbd6fc3d4d3c2132a2dc7d2f5
Cassiano <cassiano.flores2424@gmail.com>
2023-05-24 19:03:40 -0300

    sender

--- roteador/src/roteador/MessageSender.java
+++ roteador/src/roteador/MessageSender.java
@@ -4,76 +4,45 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MessageSender implements Runnable{
-    TabelaRoteamento tabela; /*Tabela de roteamento */
-    ArrayList<String> vizinhos; /* Lista de IPs dos roteadores vizinhos */
-    
-    public MessageSender(TabelaRoteamento t, ArrayList<String> v){
-        tabela = t;
-        vizinhos = v;
+public class MessageSender implements Runnable {
+
+    private static final int SEND_INTERVAL = 10000;
+    private DatagramSocket socket;
+    private TabelaRoteamento tabela;
+    private InetAddress[] vizinhos;
+
+    public MessageSender(DatagramSocket socket, TabelaRoteamento tabela, InetAddress[] vizinhos) {
+        this.socket = socket;
+        this.tabela = tabela;
+        this.vizinhos = vizinhos;
     }
-    
+
     @Override
     public void run() {
-        DatagramSocket clientSocket = null;
-        byte[] sendData;
-        InetAddress IPAddress = null;
-        
-        /* Cria socket para envio de mensagem */
-        try {
-            clientSocket = new DatagramSocket();
-        } catch (SocketException ex) {
-            Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        }
-        
-        while(true){
-            
-            /* Pega a tabela de roteamento no formato string, conforme especificado pelo protocolo. */
-            String tabela_string = tabela.get_tabela_string();
-               
-            /* Converte string para array de bytes para envio pelo socket. */
-            sendData = tabela_string.getBytes();
-            
-            /* Anuncia a tabela de roteamento para cada um dos vizinhos */
-            for (String ip : vizinhos){
-                /* Converte string com o IP do vizinho para formato InetAddress */
-                try {
-                    IPAddress = InetAddress.getByName(ip);
-                } catch (UnknownHostException ex) {
-                    Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
-                    continue;
-                }
-                
-                /* Configura pacote para envio da menssagem para o roteador vizinho na porta 5000*/
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5000);         
-                
-                /* Realiza envio da mensagem. */
-                try {
-                    clientSocket.send(sendPacket);
-                } catch (IOException ex) {
-                    Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            
-            /* Espera 10 segundos antes de realizar o próximo envio. CONTUDO, caso
-             * a tabela de roteamento sofra uma alteração, ela deve ser reenvida aos
-             * vizinho imediatamente.
-             */
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(MessageSender.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        Timer timer = new Timer();
 
-        }
-        
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                sendTabela();
+            }
+        }, 0, SEND_INTERVAL);
     }
-    
+
+    private void sendTabela() {
+        String tabelaString = tabela.getTabelaString();
+        byte[] buffer = tabelaString.getBytes();
+
+        for (InetAddress vizinho : vizinhos) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, vizinho, 5000);
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
