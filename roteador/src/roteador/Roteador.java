27c78a69ebc7dcf86cdb02fbea83b977a2c00b17
Cassiano <cassiano.flores2424@gmail.com>
2023-06-04 22:37:37 -0300

    read ip_host and neighborhood from IPVizinhos.txt

--- roteador/src/roteador/Roteador.java
+++ roteador/src/roteador/Roteador.java
@@ -7,17 +7,16 @@ import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class    Roteador {
+public class Roteador {
+
     private static final String VIZINHOS_FILE = "IPVizinhos.txt";
-    private static final String ip_host = "26.219.143.93";
+    private static String ip_host;
 
     public static void main(String[] args) {
         List<InetAddress> vizinhos = readVizinhosFromFile();
-
 
         try (DatagramSocket socket = new DatagramSocket(5000)) {
             TabelaRoteamento tabela = new TabelaRoteamento(ip_host);
@@ -33,7 +32,6 @@ public class    Roteador {
 
             receiverThread.join();
             senderThread.join();
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
@@ -42,29 +40,25 @@ public class    Roteador {
     private static List<InetAddress> readVizinhosFromFile() {
         List<InetAddress> vizinhos = new ArrayList<>();
 
-        // para testar localmente
+        URL path = Roteador.class.getResource(Roteador.VIZINHOS_FILE);
+
         try {
-            vizinhos.add(InetAddress.getByName("26.9.78.66"));
-            vizinhos.add(InetAddress.getByName("26.197.231.97"));
-            vizinhos.add(InetAddress.getByName("0.0.0.0"));
-        } catch (UnknownHostException e) {
+            File file = new File(path.getFile());
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                InetAddress address = InetAddress.getByName(line);
+
+                if (ip_host == null) {
+                    ip_host = address.toString();
+                } else {
+                    vizinhos.add(address);
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        URL path = Roteador.class.getResource(Roteador.VIZINHOS_FILE);
-//
-//        try {
-//            File file = new File(path.getFile());
-//            BufferedReader reader = new BufferedReader(new FileReader(file));
-//            String line;
-//
-//            while ((line = reader.readLine()) != null) {
-//                InetAddress address = InetAddress.getByName(line);
-//                vizinhos.add(address);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
         return vizinhos;
     }
